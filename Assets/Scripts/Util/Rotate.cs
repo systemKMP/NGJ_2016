69d27ac8a4f9af598d839561828c1adfa9afc0b5
Wesley <Wesley>
2016-04-09 21:47:41 +0200

    Added Enemy, and soccerballs

---
+++ Assets/Scripts/Util/Rotate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float RotateSpeed = 1;
+    public bool bool_rotate = true;
+    Quaternion RandomRotation;
+    public GameObject go;
+    public Vector3 RotateAxis = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        go = gameObject;
+        RandomRotation = Random.rotation;
+        if (RotateAxis != Vector3.zero)
+        {
+            RandomRotation = Quaternion.AngleAxis(10f, RotateAxis);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bool_rotate == true)
+        {
+            transform.Rotate(RandomRotation.eulerAngles * Time.deltaTime * RotateSpeed);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
