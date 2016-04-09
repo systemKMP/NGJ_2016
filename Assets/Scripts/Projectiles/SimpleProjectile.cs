08b61ed7f5c2e3e29e5bf3396d373e13c9d2b473
systemKMP <system_KMP@yahoo.com>
2016-04-09 20:20:46 +0200

    Revolver update

--- Assets/Scripts/Projectiles/SimpleProjectile.cs
+++ Assets/Scripts/Projectiles/SimpleProjectile.cs
@@ -5,7 +5,7 @@ public class SimpleProjectile : ProjectileBase {
 
     protected override void SetupVelocity()
     {
-        Velocity = transform.rotation * Vector3.forward * StartVelocity + Player.Instance.MovController._velocity * 0.95f;
+        Velocity = transform.rotation * Vector3.forward * StartVelocity + Player.Instance.MovController._velocity;
     }
 
 }
---
+++ Assets/Scripts/Weapons/Revolver.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Revolver : WeaponBase
+{
+
+    public Animator Anim;
+
+    public bool IsLoaded;
+
+    private bool firstCheck = false;
+
+    public AudioSource LoadAudio;
+    public AudioSource UnloadAudio;
+
+    protected override void NoAmmoReact()
+    {
+        UnloadAudio.Play();
+        Anim.SetBool("Loaded", false);
+        IsLoaded = false;
+        Velocities = new List<float>();
+        firstCheck = true;
+    }
+
+    public List<float> Velocities;
+    public Quaternion PreviousRotation;
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    void FixedUpdate()
+    {
+
+        if (!IsLoaded)
+        {
+            var currentRotation = transform.rotation;
+
+            float currentVelocity = 0.0f;
+
+            if (firstCheck)
+            {
+                firstCheck = false;
+            }
+            else
+            {
+                var fwd = transform.rotation * Vector3.forward;
+                var Pfwd = PreviousRotation * Vector3.forward;
+
+                var up = fwd - Pfwd;
+                currentVelocity = Vector3.Dot(transform.rotation * Vector3.up, up) / Time.fixedDeltaTime;
+                Velocities.Add(currentVelocity);
+
+
+                if (Velocities.Count >= 5)
+                {
+                    float avg = Velocities.Average();
+                    Debug.Log(avg + " - " + currentVelocity);
+                    if (avg > currentVelocity * 10.0f && avg > 5.0f)
+                    {
+                        LoadGun();
+                    }
+                }
+            }
+
+            if (Velocities.Count > 5)
+            {
+                Velocities.RemoveAt(0);
+            }
+
+            PreviousRotation = currentRotation;
+        }
+    }
+
+    private void LoadGun()
+    {
+        LoadAudio.Play();
+        _ammo = MaxAmmo;
+        IsLoaded = true;
+        Anim.SetBool("Loaded", true);
+    }
+
+
+}
