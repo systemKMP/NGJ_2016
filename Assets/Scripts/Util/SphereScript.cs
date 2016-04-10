79d347fce78ed8a48f0b9e6b07cbb1e61517e8a0
Wesley <Wesley>
2016-04-10 07:21:24 +0200

    Added new ring prefabs and ring manager

---
+++ Assets/Scripts/Util/SphereScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float z = Input.GetAxis("Horizontal")*0.5f;
+        float y = Input.GetAxis("Vertical")*0.5f;
+        float x = 0;
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            x = 1f;
+        }
+        else if(Input.GetKeyDown(KeyCode.E))
+        {
+            x = -1f;
+        }
+        transform.position = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z+z);
+	}
+}
